2d21f43b01a45de956d3e84f4f8a19b53bfb2915
ForeverCc <1615250697@qq.com>
2017-12-18 11:35:41 +0800

    remove others

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
 import AppRouter from './routes/router';
 
 import configureStore from './store/store';
